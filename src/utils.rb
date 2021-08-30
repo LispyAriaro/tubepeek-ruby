971cbbdbd6ea2c5e9ec727709be292c080296742
Efe Ariaro <efeariaroo@gmail.com>
2021-08-30 10:15:20 +0100

    Clean up utils file

--- src/utils.rb
+++ src/utils.rb
@@ -1,11 +1,3 @@
-require 'active_record'
-require 'sinatra/base'
-require 'faye/websocket'
-require 'json'
-require 'yaml'
-require_relative './migrations/run_migrations'
-require_relative './entities'
-
 module Utils
   def self.get_youtube_videoid(video_url)
     video_url
